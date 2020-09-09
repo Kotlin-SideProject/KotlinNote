8c73d9b5ba64cd71d1332818cc7ea473265339ef
Angus <pighaddt@gmail.com>
2020-09-09 22:38:09 +0800

    0909notes

---
+++ app/src/main/java/com/angus/kotlinnote/0909notes.kt
@@ -0,0 +1,9 @@
+package com.angus.kotlinnote
+
+//讀取手機上的照片
+//alt + 9 version control
+//alt + 6 logcat 快速鍵
+//git 右鍵可以比較目前程式與之前差異
+//private construct 可以不讓人()使用，需用instance方法()只有第一次呼叫，且永遠只有一個，單一物件化(singleton)
+// 第一次加入Fragment 可使用supportFragmentManager.beginTransaction().apply{add}
+//建議將fragment button 方法寫入MainActivity ，textView 在fragment onCreateView中寫成屬性
