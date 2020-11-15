77f6e1882a7acb6b30860d28ab39945d6eb4aeae
angus <pighaddt@gmail.com>
2020-11-15 16:46:53 +0800

    1114 AAD

---
+++ app/src/main/java/com/angus/kotlinnote/1114notes.kt
@@ -0,0 +1,12 @@
+package com.angus.kotlinnote
+
+//1114
+//AAD  考試要點 google 帳號登入
+// 1.付錢  2.上傳護照 3.下載plugin 4.重啟Android studio
+// Android studio 版本可以更新到4.1.1
+//準備兩套android studio ，在考試時不能開其他專案使用BATA版，copy到其他資料夾
+//6~7天interview，這個專案哪個功能最難，考試時參考那些資料，某個功能怎麼做到，描述APP有哪些功能
+// android 8.0以前不要做channel Notification
+
+//assertion ， AndoirdJUnit4ClassRunner::class 取代JUnit，測試案例會先拿車Resource
+
